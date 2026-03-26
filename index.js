c6cb1b6913f5bbdb630fe8a531f407952f87c0b2
docpilot570 <vlad250815@gmail.com>
2026-03-26 14:47:33 +0200

    create real homepage

--- index.js
+++ index.js
@@ -2,14 +2,8 @@ export default function Home() {
   return (
     <div style={{textAlign:'center', marginTop:'50px'}}>
       <h1>DocPilot AI</h1>
-      <p>Business documents in 5 minutes</p>
-
-      <a href="/demo.html">
-        <button style={{padding:15, fontSize:18}}>
-          Try Demo
-        </button>
-      </a>
-
+      <p>Business documents and reports in 5 minutes</p>
+      <a href="/demo">Try Demo</a>
     </div>
   );
 }
