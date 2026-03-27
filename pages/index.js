a049b15f85f7f19061be05c125081fbb5fc0f394
docpilot570 <vlad250815@gmail.com>
2026-03-27 14:57:35 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,3 +12,10 @@ export default function Home() {
   <a href="/privacy" style={{marginRight:15}}>Privacy</a>
   <a href="/contact">Contact</a>
 </div>
+<div style={{marginTop:30}}>
+  <a href="/demo" style={{marginRight:15}}>Demo</a>
+  <a href="/pricing" style={{marginRight:15}}>Pricing</a>
+  <a href="/terms" style={{marginRight:15}}>Terms</a>
+  <a href="/privacy" style={{marginRight:15}}>Privacy</a>
+  <a href="/contact">Contact</a>
+</div>
