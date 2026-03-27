450ac29142116d4d38f1644f239e3aa8349956dd
docpilot570 <vlad250815@gmail.com>
2026-03-27 17:23:58 +0200

    Update contact.js

--- pages/contact.js
+++ pages/contact.js
@@ -2,7 +2,7 @@ export default function Contact() {
   return (
     <div style={{maxWidth:800, margin:"40px auto", fontFamily:"Arial"}}>
       <h1>Contact</h1>
-      <p>Email: support@yourdomain.com</p>
+      <p>Email: docpilot.ai.service@gmail.com</p>
     </div>
   );
 }
