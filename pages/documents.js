f1e86c61701ecc3b7fe1f5eeee5cc116f5982ffb
docpilot570 <vlad250815@gmail.com>
2026-04-03 00:13:43 +0300

    Create documents.js

---
+++ pages/documents.js
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <meta name="verify-paysera" content="58db8407c727c94c67bf7a0b636b165a">
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
