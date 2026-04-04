a4d25abbf30954f18d28f5324b9a878f67b2a8ed
docpilot570 <vlad250815@gmail.com>
2026-04-04 15:15:29 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,10 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <meta name="verify-paysera" content="58db8407c727c94c67bf7a0b636b165a" />
+        <meta
+          name="verify-paysera"
+          content="58db8407c727c94c67bf7a0b636b165a"
+        />
       </Head>
 
       <Component {...pageProps} />
