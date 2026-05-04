1bed552a6432d9bed804259884e3b5bf63f65358
docpilot570 <vlad250815@gmail.com>
2026-05-05 00:15:59 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,18 +2,18 @@ import Head from "next/head";
 import Footer from "../components/Footer";
 
 export default function App({ Component, pageProps }) {
-  return (
+  возвращаться (
     <>
-      <Head>
-        <meta
+      <Голова>
+        <мета
           name="verify-paysera"
-          content="58db8407c727c94c67bf7a0b636b165a"
+          содержание = " 58db8407c727c94c67bf7a0b636b16 5a"
         />
       </Head>
 
       <Component {...pageProps} />
       
-      <Footer />
+      <Нижний колонтитул />
     </>
   );
 }
